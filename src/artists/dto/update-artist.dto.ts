40d623e20fd5699dc73db028cb98a5a11f952d51
thirdmadman <thirdmadman@gmail.com>
2023-07-26 14:27:11 +0200

    feat: add artist dto

--- src/artists/dto/update-artist.dto.ts
+++ src/artists/dto/update-artist.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 import { CreateArtistDto } from './create-artist.dto';
 
-export class UpdateArtistDto extends PartialType(CreateArtistDto) {}
+export class UpdateArtistDto extends PartialType(CreateArtistDto) {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsBoolean()
+  grammy: boolean;
+}
