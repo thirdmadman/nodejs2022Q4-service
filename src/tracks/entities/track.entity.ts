1dcfad8c0a4ba22f9a250ce006e9bda353ab80ec
thirdmadman <thirdmadman@gmail.com>
2023-07-26 18:53:28 +0200

    feat: update TrackEntity

--- src/tracks/entities/track.entity.ts
+++ src/tracks/entities/track.entity.ts
@@ -1,1 +1,11 @@
-export class Track {}
+export class TrackEntity {
+  id: string; // uuid v4
+  name: string;
+  artistId: string | null; // refers to Artist
+  albumId: string | null; // refers to Album
+  duration: number; // integer number
+
+  constructor(partial: Partial<TrackEntity>) {
+    Object.assign(this, partial);
+  }
+}
