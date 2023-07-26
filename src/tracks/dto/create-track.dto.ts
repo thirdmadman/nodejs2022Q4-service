2939036846bd2f49052871a9f5d951365bbe4e4a
thirdmadman <thirdmadman@gmail.com>
2023-07-26 18:53:17 +0200

    feat: add track dto

--- src/tracks/dto/create-track.dto.ts
+++ src/tracks/dto/create-track.dto.ts
@@ -1,1 +1,25 @@
-export class CreateTrackDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateTrackDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  artistId: string | null; // refers to Artist
+
+  @IsString()
+  @IsOptional()
+  albumId: string | null; // refers to Album
+
+  @IsNumber()
+  @Min(0)
+  duration: number; // integer number
+}
--- src/tracks/dto/update-track.dto.ts
+++ src/tracks/dto/update-track.dto.ts
@@ -1,4 +1,27 @@
 import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
 import { CreateTrackDto } from './create-track.dto';
 
-export class UpdateTrackDto extends PartialType(CreateTrackDto) {}
+export class UpdateTrackDto extends PartialType(CreateTrackDto) {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  artistId: string | null; // refers to Artist
+
+  @IsString()
+  @IsOptional()
+  albumId: string | null; // refers to Album
+
+  @IsNumber()
+  @Min(0)
+  duration: number; // integer number
+}
