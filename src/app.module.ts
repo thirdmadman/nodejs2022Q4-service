25590d924924097fd6c23224aa49e98933012581
thirdmadman <thirdmadman@gmail.com>
2023-07-26 11:12:33 +0200

    feat: update app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AlbumsModule } from './albums/albums.module';
+import { ArtistsModule } from './artists/artists.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { TracksModule } from './tracks/tracks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AlbumsModule,
+    ArtistsModule,
+    FavoritesModule,
+    TracksModule,
+    UsersModule,
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
