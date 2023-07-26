145a178c3039a5a6f0dd41f66e83ebd9e3edb8d3
thirdmadman <thirdmadman@gmail.com>
2023-07-26 18:41:16 +0200

    feat: impl AlbumEntity

--- src/albums/entities/album.entity.ts
+++ src/albums/entities/album.entity.ts
@@ -1,1 +1,10 @@
-export class Album {}
+export class AlbumEntity {
+  id: string;
+  name: string;
+  artistId: string | null;
+  year: number;
+
+  constructor(partial: Partial<AlbumEntity>) {
+    Object.assign(this, partial);
+  }
+}
