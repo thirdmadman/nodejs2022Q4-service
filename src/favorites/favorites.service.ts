51809479691f1173f3dc71d4f62eeb470d9ffa08
thirdmadman <thirdmadman@gmail.com>
2023-07-27 15:48:40 +0200

    feat: impl FavoritesService

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,26 +1,124 @@
+import { artistRepository } from './../artists/artist.repository';
+import { favoriteRepository } from './favorite.repository';
 import { Injectable } from '@nestjs/common';
-import { CreateFavoriteDto } from './dto/create-favorite.dto';
-import { UpdateFavoriteDto } from './dto/update-favorite.dto';
+import { ArtistEntity } from 'src/artists/entities/artist.entity';
+import { AlbumEntity } from 'src/albums/entities/album.entity';
+import { TrackEntity } from 'src/tracks/entities/track.entity';
+import { FavoriteEntity } from './entities/favorite.entity';
+import { trackRepository } from 'src/tracks/track.repository';
+import { albumRepository } from 'src/albums/album.repository';
 
 @Injectable()
 export class FavoritesService {
-  create(createFavoriteDto: CreateFavoriteDto) {
-    return 'This action adds a new favorite';
-  }
-
   findAll() {
-    return `This action returns all favorites`;
+    const favoritesSource = favoriteRepository.findAll();
+
+    if (!favoritesSource) return null;
+
+    const result: FavoriteEntity = {
+      artists: new Array<ArtistEntity>(),
+      albums: new Array<AlbumEntity>(),
+      tracks: new Array<TrackEntity>(),
+    };
+
+    if (favoritesSource.length === 0) {
+      return result;
+    }
+
+    for (let i = 0; i < favoritesSource.length; i++) {
+      const source = favoritesSource[i];
+      if (source.artistId !== null) {
+        const artist = artistRepository.findOne(source.artistId);
+        if (artist) {
+          result.artists.push(new ArtistEntity(artist));
+        }
+        continue;
+      }
+      if (source.albumId !== null) {
+        const album = albumRepository.findOne(source.albumId);
+        if (album) {
+          result.albums.push(new AlbumEntity(album));
+        }
+        continue;
+      }
+      if (source.trackId !== null) {
+        const track = trackRepository.findOne(source.trackId);
+        if (track) {
+          result.tracks.push(new TrackEntity(track));
+        }
+        continue;
+      }
+    }
+
+    return result;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} favorite`;
+  createFavoriteTrack(id: string) {
+    const track = trackRepository.findOne(id);
+    if (!track) {
+      return null;
+    }
+    const link = favoriteRepository.create({
+      id: '',
+      userId: null,
+      albumId: null,
+      artistId: null,
+      trackId: id,
+    });
+    return link ? { isSuccess: true } : { isSuccess: false };
   }
 
-  update(id: string, updateFavoriteDto: UpdateFavoriteDto) {
-    return `This action updates a #${id} favorite`;
+  removeFavoriteTrack(id: string) {
+    const links = favoriteRepository.findAllWhere({ trackId: id });
+    if (!links) return null;
+
+    links.forEach((link) => favoriteRepository.delete(link.id));
+    return true;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} favorite`;
+  createFavoriteAlbum(id: string) {
+    const album = albumRepository.findOne(id);
+    if (!album) {
+      return null;
+    }
+    const link = favoriteRepository.create({
+      id: '',
+      userId: null,
+      albumId: id,
+      artistId: null,
+      trackId: null,
+    });
+    return link ? { isSuccess: true } : { isSuccess: false };
+  }
+
+  removeFavoriteAlbum(id: string) {
+    const links = favoriteRepository.findAllWhere({ albumId: id });
+    if (!links) return null;
+
+    links.forEach((link) => favoriteRepository.delete(link.id));
+    return true;
+  }
+
+  createFavoriteArtist(id: string) {
+    const artist = artistRepository.findOne(id);
+    if (!artist) {
+      return null;
+    }
+    const link = favoriteRepository.create({
+      id: '',
+      userId: null,
+      albumId: null,
+      artistId: id,
+      trackId: null,
+    });
+    return link ? { isSuccess: true } : { isSuccess: false };
+  }
+
+  removeFavoriteArtist(id: string) {
+    const links = favoriteRepository.findAllWhere({ artistId: id });
+    if (!links) return null;
+
+    links.forEach((link) => favoriteRepository.delete(link.id));
+    return true;
   }
 }
