f3a96b112c9ab2251986be1994bde5d0793c8b09
thirdmadman <thirdmadman@gmail.com>
2023-07-27 15:47:57 +0200

    feat: update FavoriteEntity

--- src/favorites/entities/favorite.entity.ts
+++ src/favorites/entities/favorite.entity.ts
@@ -1,1 +1,13 @@
-export class Favorite {}
+import { AlbumEntity } from 'src/albums/entities/album.entity';
+import { ArtistEntity } from 'src/artists/entities/artist.entity';
+import { TrackEntity } from 'src/tracks/entities/track.entity';
+
+export class FavoriteEntity {
+  artists: Array<ArtistEntity>;
+  albums: Array<AlbumEntity>;
+  tracks: Array<TrackEntity>;
+
+  constructor(partial: Partial<TrackEntity>) {
+    Object.assign(this, partial);
+  }
+}
