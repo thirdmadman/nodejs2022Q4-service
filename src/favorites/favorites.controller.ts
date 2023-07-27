0ca22cab02dd4c4de7fbd8144196613fce18b2af
thirdmadman <thirdmadman@gmail.com>
2023-07-27 15:49:10 +0200

    feat: impl favorites controller

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -2,49 +2,75 @@ import {
   Controller,
   Get,
   Post,
-  Body,
-  Put,
   Param,
   Delete,
   UseInterceptors,
   ClassSerializerInterceptor,
   HttpCode,
   ParseUUIDPipe,
+  NotFoundException,
+  UnprocessableEntityException,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
-import { CreateFavoriteDto } from './dto/create-favorite.dto';
-import { UpdateFavoriteDto } from './dto/update-favorite.dto';
 @UseInterceptors(ClassSerializerInterceptor)
-@Controller('favorites')
+@Controller('favs')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
-
-  @Post()
-  create(@Body() createFavoriteDto: CreateFavoriteDto) {
-    return this.favoritesService.create(createFavoriteDto);
-  }
 
   @Get()
   findAll() {
     return this.favoritesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.favoritesService.findOne(id);
+  @Post('/track/:id')
+  createFavoriteTrack(@Param('id', ParseUUIDPipe) id: string) {
+    const result = this.favoritesService.createFavoriteTrack(id);
+    if (!result) throw new UnprocessableEntityException();
+    if (!result.isSuccess) throw new InternalServerErrorException();
+    return result;
   }
 
-  @Put(':id')
-  update(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Body() updateFavoriteDto: UpdateFavoriteDto,
-  ) {
-    return this.favoritesService.update(id, updateFavoriteDto);
-  }
-
-  @Delete(':id')
+  @Delete('/track/:id')
   @HttpCode(204)
-  remove(@Param('id', ParseUUIDPipe) id: string) {
-    return this.favoritesService.remove(id);
+  removeFavoriteTrack(@Param('id', ParseUUIDPipe) id: string) {
+    const result = this.favoritesService.removeFavoriteTrack(id);
+    if (result === null) throw new NotFoundException();
+    if (!result) throw new InternalServerErrorException();
+    return result;
+  }
+
+  @Post('/album/:id')
+  createFavoriteAlbum(@Param('id', ParseUUIDPipe) id: string) {
+    const result = this.favoritesService.createFavoriteAlbum(id);
+    if (!result) throw new UnprocessableEntityException();
+    if (!result.isSuccess) throw new InternalServerErrorException();
+    return result;
+  }
+
+  @Delete('/album/:id')
+  @HttpCode(204)
+  removeFavoriteAlbum(@Param('id', ParseUUIDPipe) id: string) {
+    const result = this.favoritesService.removeFavoriteAlbum(id);
+    if (result === null) throw new NotFoundException();
+    if (!result) throw new InternalServerErrorException();
+    return result;
+  }
+
+  @Post('/artist/:id')
+  createFavoriteArtist(@Param('id', ParseUUIDPipe) id: string) {
+    const result = this.favoritesService.createFavoriteArtist(id);
+    if (!result) throw new UnprocessableEntityException();
+    if (!result.isSuccess) throw new InternalServerErrorException();
+    return result;
+  }
+
+  @Delete('/artist/:id')
+  @HttpCode(204)
+  removeFavoriteArtist(@Param('id', ParseUUIDPipe) id: string) {
+    const result = this.favoritesService.removeFavoriteArtist(id);
+    if (result === null) throw new NotFoundException();
+    if (!result) throw new InternalServerErrorException();
+    return result;
   }
 }
