5af5c0a3a0f04cc0631dee6f27996819d6ac2450
thirdmadman <thirdmadman@gmail.com>
2023-08-14 16:54:13 +0200

    refactor: make code more readable

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,48 +13,59 @@ export class UsersService {
       data: {
         login: createUserDto.login,
         password: createUserDto.password,
-        version: 1,
       },
     });
+
     if (!user) return null;
+
     const convertedUser = {
       ...user,
       createdAt: user.createdAt.getTime(),
       updatedAt: user.updatedAt.getTime(),
     };
+
     return new UserEntity(convertedUser);
   }
 
   async findAll() {
     const users = await this.prisma.user.findMany();
+
     return users.map((src) => {
       if (!src) return null;
+
       const convertedUser = {
         ...src,
         createdAt: src.createdAt.getTime(),
         updatedAt: src.updatedAt.getTime(),
       };
+
       return new UserEntity(convertedUser);
     });
   }
 
   async findOne(id: string) {
     const user = await this.prisma.user.findUnique({ where: { id } });
+
     if (!user) return null;
+
     const convertedUser = {
       ...user,
       createdAt: user.createdAt.getTime(),
       updatedAt: user.updatedAt.getTime(),
     };
+
     return new UserEntity(convertedUser);
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
     const user = await this.prisma.user.findUnique({ where: { id } });
+
     if (!user) return { entity: null };
+
     if (user.password !== updateUserDto.oldPassword) {
       return { isPasswordMismatch: true };
     }
+
     const updatedUser = await this.prisma.user.update({
       where: { id },
       data: {
@@ -62,26 +73,32 @@ export class UsersService {
         version: user.version + 1,
       },
     });
+
     if (!updatedUser) return null;
+
     const convertedUser = {
       ...updatedUser,
       createdAt: updatedUser.createdAt.getTime(),
       updatedAt: updatedUser.updatedAt.getTime(),
     };
+
     return { entity: new UserEntity(convertedUser) };
   }
 
   async remove(id: string) {
     const isUser = await this.prisma.user.findUnique({ where: { id } });
+
     if (!isUser) return null;
 
     const user = await this.prisma.user.delete({ where: { id } });
+
     if (!user) return null;
     const convertedUser = {
       ...user,
       createdAt: user.createdAt.getTime(),
       updatedAt: user.updatedAt.getTime(),
     };
+
     return new UserEntity(convertedUser);
   }
 }
